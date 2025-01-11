910ec93cbbd3fad487351036ea6bbad241dd6486
gadbend <gadbendavid@outlook.com>
2025-01-11 16:25:10 +0000

    Deploying to gh-pages from @ gadbend/toygerparadise@656303e36533251d69189377cbb640726744e57c 🚀

--- assets/browser-CJy1-Lmo.js
+++
@@ -1,2 +0,0 @@
-import{D as e}from"./index-CbzG7VsJ.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
-//# sourceMappingURL=browser-CJy1-Lmo.js.map
